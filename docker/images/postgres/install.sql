f758f7ba87f4c72719f87ffa91ee5b6bf83763cb
Guilherme Müller <guilherme.muller.m@gmail.com>
2020-06-09 13:33:53 -0300

    updates on the postgre installation sgl

--- docker/images/postgres/install.sql
+++ docker/images/postgres/install.sql
@@ -2,10 +2,11 @@ CREATE DATABASE gert;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE resource_owner_tb (id SERIAL PRIMARY KEY, name VARCHAR(200), password VARCHAR(255));
+/* Authentication Scope Entities */
+CREATE SCHEMA authentication;
 
-CREATE TABLE client_tb (id UUID DEFAULT uuid_generate_v4(), name VARCHAR(200), resource_owner INT REFERENCES resource_owner_tb(id) );
+CREATE TABLE authentication.resource_owner_tb (id SERIAL PRIMARY KEY, name VARCHAR(200), password VARCHAR(255));
+CREATE TABLE authentication.client_tb (id UUID DEFAULT uuid_generate_v4(), name VARCHAR(200), resource_owner INT REFERENCES authentication.resource_owner_tb(id) );
+CREATE TABLE authentication.access_token_tb(id UUID, created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(), expires_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() + INTERVAL '6 hours');
+CREATE TABLE authentication.application_scope_tb(id VARCHAR(255) UNIQUE NOT NULL, name VARCHAR(255), description VARCHAR(255));
 
-CREATE TABLE access_token_tb(id UUID, created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(), expires_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() + INTERVAL '6 hours');
-
-CREATE TABLE application_scope_tb(id VARCHAR(255) UNIQUE NOT NULL, name VARCHAR(255), description VARCHAR(255));
